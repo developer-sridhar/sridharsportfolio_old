cb49090978313cef50078862fcd28fcb579cab76
developer-sridhar <140785207+developer-sridhar@users.noreply.github.com>
2024-07-29 00:39:59 +0530

    Update AdminIntro.js

--- frontend/src/pages/Home/admin/AdminIntro.js
+++ frontend/src/pages/Home/admin/AdminIntro.js
@@ -11,7 +11,7 @@ const AdminIntro = () => {
   const onFinish = async(values) => {
     try {
       dispatch(ShowLoading())
-      const response = await axios.post("/api/portfolio/update-intro", {
+      const response = await axios.post("https://sridharsportfolio.onrender.com/api/portfolio/update-intro", {
         ...values,
         _id: portfolioData.intro._id,
       });
